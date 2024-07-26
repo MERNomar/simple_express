6fae8ea73e9581485706d9e06c7d64712a922e6d
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-07-26 19:16:24 +0300

    edited index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  res.sendStatus(200);
 });
 
 app.get("/", (req, res) => {
-  res.sendStatus(200);
+  console.log("get requested");
+  res.json({ welcome: "welcome" });
 });
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log("everytihng is up and running");
 });
