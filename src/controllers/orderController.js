6cda5fa521258e14ae3f14fbbc6d5ae76250a29a
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-07-26 20:46:53 +0300

    adding tracking number

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -5,7 +5,8 @@ const getAllOrders = (req, res) => {
 
 const postOrder = (req, res) => {
   console.log(req.body);
-  res.sendStatus(200);
+  const rando = Math.floor(Math.random() * 9999);
+  res.json({ tracking_number: rando }).status(200);
 };
 
 module.exports = { getAllOrders, postOrder };
